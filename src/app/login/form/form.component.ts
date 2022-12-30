e27dcd5141ad360dfbce2f593b4a06956583d3fa
Emerson Suarez <emersonsuarez2904@gmail.com>
2022-12-30 10:02:01 -0500

    Validación simple

--- src/app/login/form/form.component.ts
+++ src/app/login/form/form.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  user : string = 'Angular';
+  password : string = '123456';
+  nav : string[] = ['Home', 'Support', 'Contact'];
+  mostrar : boolean = true;
+  bienvenida : boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(usuario:string,pass:string){
+    if((usuario === this.user) && (pass === this.password) ){
+      this.bienvenida = true;
+    }else{
+      this.mostrar = false;
+    }
+  }
 }
